77a76ae1195f70a7b20b38dfa78973b828335624
kanalsotiladi07-svg <kanalsotiladi07@gmail.com>
2026-01-21 16:44:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom 👋 Bot ishlayapti!")
+
+app = ApplicationBuilder().token("8226108395:AAHltktOTXPztHTDy826ernq2JZrDZRt5Qo").build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
